8b88511784ef9f3ff9232ce0a7c20cf22a4ef79a
Rameshwar <bhagwatrameshwar2000@gmail.com>
2025-07-31 20:05:30 +0530

    make function of cart

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,18 @@
 export const cart=[];
+
+export function addToCart(productId) {
+  let matchingid;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.id) {
+      matchingid = cartItem;
+    }
+  });
+  if (matchingid) {
+    matchingid.quantity += 1;
+  } else {
+    cart.push({
+      id: productId,
+      quantity: 1,
+    });
+  }
+}
