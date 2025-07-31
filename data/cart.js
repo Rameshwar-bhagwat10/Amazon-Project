1a797f017b153f453a158a117268be6e0b1b4433
Rameshwar <bhagwatrameshwar2000@gmail.com>
2025-07-31 22:44:44 +0530

    added delete cart feature

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,16 @@
-export const cart=[
+export let cart = [
   {
-  productId:'83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-  quantity:2,
-},
- {
-  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity:2,
-},
-{
-  productId:'54e0eccd-8f36-462b-b68a-8182611d9add',
-  quantity:3
-}
+    productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    quantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 2,
+  },
+  {
+    productId: "54e0eccd-8f36-462b-b68a-8182611d9add",
+    quantity: 3,
+  },
 ];
 
 export function addToCart(productId) {
@@ -28,4 +28,15 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+  cart=newCart;
 }
